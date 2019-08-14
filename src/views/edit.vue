9d014aec814b05f253c6784dc6a76a446d05f1b2
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-08-14 10:39:03 +0800

    init

---
+++ src/views/edit.vue
@@ -0,0 +1,12 @@
+<template lang='pug'>
+  .edit
+</template>
+
+<script>
+  export default {
+    name: 'edit'
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,12 @@
+<template lang='pug'>
+  .index
+</template>
+
+<script>
+  export default {
+    name: 'index'
+  }
+</script>
+<style lang="scss" scoped>
+
+</style>
