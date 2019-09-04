f994b0d98eecb1cd37ad9c1c06213762beb51bc0
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-09-04 16:26:30 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import './axios-interceptors'
+import './api/interceptor'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
